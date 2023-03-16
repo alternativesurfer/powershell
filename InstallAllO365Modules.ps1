1f15ef8b77d19c0083ea50e521cf524d18addb05
alternativesurfer <hNSaChJ4zbrct2qx+github@protonmail.com>
2023-03-16 10:04:13 -0700

    Create InstallAllO365Modules.ps1

---
+++ InstallAllO365Modules.ps1
@@ -0,0 +1,7 @@
+Install-Module -Name ExchangeOnlineManagement
+Install-Module –Name MSOnline
+Install-Module –Name AzureAD
+Install-Module –Name Microsoft.Online.SharePoint.PowerShell
+Install-Module -Name SharePointPnPPowerShellOnline
+Install-Module –Name MicrosoftTeams
+Install-Module -Name PartnerCenter -AllowClobber -Scope AllUsers
