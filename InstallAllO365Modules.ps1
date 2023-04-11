956c03413a6404b991e3d851b9b0fd95bd91a176
alternativesurfer <hNSaChJ4zbrct2qx+github@protonmail.com>
2023-04-11 11:56:14 -0700

    Update InstallAllO365Modules.ps1
    Signed-off-by: alternativesurfer <hNSaChJ4zbrct2qx+github@protonmail.com>

--- InstallAllO365Modules.ps1
+++ InstallAllO365Modules.ps1
@@ -5,3 +5,5 @@ Install-Module –Name Microsoft.Online.SharePoint.PowerShell
 Install-Module -Name SharePointPnPPowerShellOnline
 Install-Module –Name MicrosoftTeams
 Install-Module -Name PartnerCenter -AllowClobber -Scope AllUsers
+Install-Module -Name Microsoft.Graph.Intune
+Install-Module -Name IntuneBackupAndRestore
