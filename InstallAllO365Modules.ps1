ee9559129877f8604e92c09c2e91924b05b4f056
alternativesurfer <hNSaChJ4zbrct2qx+github@protonmail.com>
2023-05-10 18:19:47 +0000

    Update InstallAllO365Modules.ps1
    Signed-off-by: alternativesurfer <hNSaChJ4zbrct2qx+github@protonmail.com>

--- InstallAllO365Modules.ps1
+++ InstallAllO365Modules.ps1
@@ -3,22 +3,22 @@ Set-ExecutionPolicy unrestricted
 
 ##### Install
 Install-Module -Name ExchangeOnlineManagement
-Install-Module –Name MSOnline
-Install-Module –Name AzureAD
-Install-Module –Name Microsoft.Online.SharePoint.PowerShell
+Install-Module -Name MSOnline
+Install-Module -Name AzureAD
+Install-Module -Name Microsoft.Online.SharePoint.PowerShell
 Install-Module -Name SharePointPnPPowerShellOnline
-Install-Module –Name MicrosoftTeams
+Install-Module -Name MicrosoftTeams
 Install-Module -Name PartnerCenter -AllowClobber -Scope AllUsers
 Install-Module -Name Microsoft.Graph.Intune
 Install-Module -Name IntuneBackupAndRestore
 
 ##### Import
 Import-Module -Name ExchangeOnlineManagement
-Import-Module –Name MSOnline
-Import-Module –Name AzureAD
-Import-Module –Name Microsoft.Online.SharePoint.PowerShell
+Import-Module -Name MSOnline
+Import-Module -Name AzureAD
+Import-Module -Name Microsoft.Online.SharePoint.PowerShell
 Import-Module -Name SharePointPnPPowerShellOnline
-Import-Module –Name MicrosoftTeams
+Import-Module -Name MicrosoftTeams
 Import-Module -Name PartnerCenter -AllowClobber -Scope AllUsers
 Import-Module -Name Microsoft.Graph.Intune
 Import-Module -Name IntuneBackupAndRestore
